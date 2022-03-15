cf1fa74ca70ec79e63ab1e9e6693bc990906f16e
Mustafa <mustafaislamovich@gmail.com>
2022-03-15 17:40:53 +0100

    b

--- index.php
+++ index.php
@@ -3,4 +3,20 @@
 echo "Hello ";
 echo "Mustafa";
 
+
+$servername = "localhost";
+$username = "webdesignchallenges";
+$password = "webdesignchallenges";
+$schema = "webdesignchallenges";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
  ?>
