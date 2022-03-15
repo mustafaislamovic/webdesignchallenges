ad4eff54294e7691a31a477b82dad7636ef56420
Mustafa <mustafaislamovich@gmail.com>
2022-03-15 17:47:30 +0100

    a

--- index.php
+++ index.php
@@ -14,6 +14,11 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
+
+  $stmt = $conn->prepare("SELECT * FROM tableone;");
+  $result = $stmt->execute();
+  print_r($result);
+
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
