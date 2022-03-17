51bee4a459c396c29ab2f676b57998dcbe56250e
Mustafa <mustafaislamovich@gmail.com>
2022-03-17 14:51:05 +0100

    managing code

--- index.php
+++ index.php
@@ -4,28 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 echo "Hello ";
-echo "Mustafa";
+require_once("rest/DAO/projectDAO.class.php");
 
-
-$servername = "localhost";
-$username = "webdesignchallenges";
-$password = "webdesignchallenges";
-$schema = "webdesignchallenges";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-
-  $stmt = $conn->prepare("SELECT * FROM tableone;");
-  $stmt->execute();
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  print_r($result);
-
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
-
+$dao = new projectDao();
+$results = $dao->get_all();
+print_r($results);
  ?>
