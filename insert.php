1262ea5677e90f13372b067b531ae1028a9e76ae
Mustafa <mustafaislamovich@gmail.com>
2022-03-17 15:27:17 +0100

    simplify

--- insert.php
+++ insert.php
@@ -4,28 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";
-$username = "webdesignchallenges";
-$password = "webdesignchallenges";
-$schema = "webdesignchallenges";
+require_once("rest(backend)/DAO/projectDAO.class.php");
+
+$description = $_REQUEST['description']
 
 
-
-$description = $_REQUEST['description'];
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$schema", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-
-  $stmt = $conn->prepare("INSERT INTO tableone(description, created) VALUES('$description', '2022-03-14 12:00:00')");
-  $result = $stmt->execute();
-  print_r($result);
-
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
+$dao = new projectDao();
+$results = $dao->add($description);
+print_r($results);
 
 ?>
