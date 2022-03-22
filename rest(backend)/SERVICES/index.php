ff093adeddf5e55825447cb2dc49eb005469f80d
Mustafa <mustafaislamovich@gmail.com>
2022-03-22 18:28:08 +0100

    12312

--- rest(backend)/SERVICES/index.php
+++ rest(backend)/SERVICES/index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'DAO/projectDao.class.php';
+require_once '../DAO/projectDao.class.php';
 require_once '../../vendor/autoload.php';
 // Crud operations for project entity
 
