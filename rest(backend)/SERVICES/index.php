0929eb4de12da89f45b5b1b243fb7e5bdba66cd3
Mustafa <mustafaislamovich@gmail.com>
2022-03-22 18:08:16 +0100

    //list all things

--- rest(backend)/SERVICES/index.php
+++ rest(backend)/SERVICES/index.php
@@ -4,6 +4,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require '../../vendor/autoload.php';
+// Crud operations for project entity
+
+//list all things
+Flight::route('/webdesignchallenges', function (){
+  echo "Hello there";
+});
+//list individual things
+$dao = new projectDao();
+$webdesignchallenges = $dao->get_all();
+print_r($webdesignchallenges)
+//add thing
+
+//update thing
+
+//delete thing
 
 Flight::route('/', function (){
   echo "Hello there";
